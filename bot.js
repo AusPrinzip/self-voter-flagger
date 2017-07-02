7ae2c81b3439ce5df0484cd6f4b8d33262d766b0
thrize <thrize@tutanota.de>
2017-07-02 22:35:58 +0100

    Use @gtg RPC for Steem API access

--- bot.js
+++ bot.js
@@ -41,7 +41,7 @@ mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
 });
 
 function main() {
-  //steem.config.set('websocket','wss://steemd.steemit.com');
+  steem.config.set('websocket','wss://gtg.steem.house:8090');
   init(function () {
     getLastInfos(function () {
       doProcess(mLastInfos.lastBlock + 1, function () {
