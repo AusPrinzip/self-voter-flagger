e75d6b1002bdd2e3cff6d444ad06e9ff5d5e78dc
thrize <thrize@tutanota.de>
2017-07-11 00:37:15 +0100

    Fix for each usage again

--- create_flag_queue.js
+++ create_flag_queue.js
@@ -26,88 +26,57 @@ function main() {
 
 
 function createQueue(steemPerRshare, callback) {
-  wait.launchFiber(function() {
-    lib.getAllVoters_reset();
-    console.log("getting voters...");
-    var keepGoing = true;
-    var posts = [];
-    while(keepGoing) {
-      var voters = [];
-      try {
-        var voters_item = wait.for(lib.getEachVoter);
-        voters.push(voters_item);
-      } catch(err) {
-        console.log("No more posts to get (mongodb threw err)");
-        keepGoing = false;
-        break;
-      }
-      if (voters === null || voters === undefined || voters.length <= 0) {
-        console.log("No more posts to get (nothing returned)");
-        keepGoing = false;
-        break;
-      }
-      console.log("processing "+voters.length+" voters...");
-      for (var i = 0 ; i < voters.length ; i++) {
-        console.log(" - voter: "+voters[i].voter);
-        if (voters[i].selfvotes_detail_daily.length > 0) {
-          for (var j = 0 ; j < voters[i].selfvotes_detail_daily.length ; j++) {
-            console.log(" - - self vote "+j);
-            var toAdd = false;
-            if (posts.length >= 4) {
-              for (var k = 0 ; k < posts.length ; k++) {
-                if (posts[k].rshares < voters[i].selfvotes_detail_daily[j].rshares) {
-                  console.log(" - - - removing post "+posts[k].permlink+" at rhsares "+posts[k].rshares);
-                  posts = posts.splice(k, 1);
-                  toAdd = true;
-                  break;
-                }
-              }
-            } else {
-              console.log(" - - - less than 4 posts");
-              toAdd = true;
-            }
-
-            if (toAdd) {
-              var post = {
-                author: voters[i].voter,
-                permlink: voters[i].selfvotes_detail_daily[j].permlink,
-                rshares: voters[i].selfvotes_detail_daily[j].rshares,
-                rsteem: steemPerRshare * voters[i].selfvotes_detail_daily[j].rshares
-              };
-              console.log(" - - - adding "+JSON.stringify(post));
-              posts.push(post);
-            }
-            // copy daily to weekly stats (will be wiped afterward)
-            voters[i].selfvotes_detail_weekly.push(voters[i].selfvotes_detail_daily[j]);
-          }
-          voters[i].selfvotes_detail_daily = []; //reset daily permlink
-        } else {
-          console.log(" - doesnt have votes, skipping");
+  lib.getAllVoters_reset();
+  console.log("getting voters...");
+  var posts = [];
+  lib.getEachVoter(function (err, item) {
+    if (item === null) {
+      console.log("No more posts to get");
+      console.log(" RESULTS: " + JSON.stringify(posts));
+      wait.launchFiber(function () {
+        for (var i = 0; i < posts.length; i++) {
+          wait.for(lib.mongoSave_wrapper, lib.DB_QUEUE, posts[i]);
         }
+        callback();
+      });
+    }
+    console.log(" - voter: " + item.voter);
+    if (item.selfvotes_detail_daily.length > 0) {
+      for (var j = 0; j < item.selfvotes_detail_daily.length; j++) {
+        console.log(" - - self vote " + j);
+        var toAdd = false;
+        if (posts.length >= 4) {
+          for (var k = 0; k < posts.length; k++) {
+            if (posts[k].rshares < item.selfvotes_detail_daily[j].rshares) {
+              console.log(" - - - removing post " + posts[k].permlink + " at rhsares " + posts[k].rshares);
+              posts = posts.splice(k, 1);
+              toAdd = true;
+              break;
+            }
+          }
+        } else {
+          console.log(" - - - less than 4 posts");
+          toAdd = true;
+        }
+
+        if (toAdd) {
+          var post = {
+            author: item.voter,
+            permlink: item.selfvotes_detail_daily[j].permlink,
+            rshares: item.selfvotes_detail_daily[j].rshares,
+            rsteem: steemPerRshare * item.selfvotes_detail_daily[j].rshares
+          };
+          console.log(" - - - adding " + JSON.stringify(post));
+          posts.push(post);
+        }
+        // copy daily to weekly stats (will be wiped afterward)
+        item.selfvotes_detail_weekly.push(item.selfvotes_detail_daily[j]);
       }
+      item.selfvotes_detail_daily = []; //reset daily permlink
+      // TODO : save item
+    } else {
+      console.log(" - doesnt have votes, skipping");
     }
-
-    console.log(" RESULTS: "+JSON.stringify(posts));
-
-    for (var i = 0 ; i < posts.length ; i++) {
-      wait.for(lib.mongoSave_wrapper, lib.DB_QUEUE, posts[i]);
-    }
-
-    /*
-     wait.for(mongoSave_wrapper, DB_RUNS,
-     {
-     start_block: startAtBlockNum,
-     end_block: mProperties.head_block_number,
-     votes_total: totalVotes,
-     selfvotes_total: numSelfVotes,
-     selfvotes_comments: numSelfComments,
-     selfvotes_high_sp_comments: numSelfVotesToProcess,
-     flags_cancelled: numFlagsToCancel
-     });
-     mLastInfos.lastBlock = mProperties.head_block_number;
-     wait.for(mongoSave_wrapper, DB_RECORDS, mLastInfos);
-     */
-    callback();
   });
 }
 
