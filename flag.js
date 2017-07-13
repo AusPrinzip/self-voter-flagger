b78ceb9ac3fcee225ea70e85d5f5590b5b52926f
thrize <thrize@tutanota.de>
2017-07-13 15:54:04 +0100

    Revert "Add debug cycling through all queue"
    This reverts commit 25baf4d7f548967decc1ff57d27d0b078add2bd9.

--- flag.js
+++ flag.js
@@ -69,7 +69,7 @@ function doProcess(callback) {
     while (queue.length > 0 && count < MAX_ITERATIONS) {
       count++;
       // process ONE item
-      var item = queue[count % queue.length];
+      var item = queue[0];
 
       // check payout window still open (only when active)
       if (process.env.ACTIVE !== undefined
