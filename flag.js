228e6ba55ac6cfb244496d61b65205d0f7590ddb
thrize <thrize@tutanota.de>
2017-08-03 10:06:34 +0100

    Use cal method calc for flag weight

--- flag.js
+++ flag.js
@@ -156,7 +156,10 @@ function doProcess(callback) {
       // METHOD 2 - cal
       console.log("METHOD 2");
       var vpCal = abs_need_rshares / ((totalVests) * 0.02 * (vp / 100));
-      console.log("vapCal: "+(vpCal * -1));
+      console.log("vapCal: "+vpCal);
+
+      counter_percentage = ((vpCal / 100) * -1);
+      console.log("method 2 countering percentage: " + counter_percentage);
 
 
       console.log("Voting...");
